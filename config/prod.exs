76caa92aa5371ef5855430f688292d2533bc69a1
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2026-04-09 14:52:52 -0300

    Adding deployex configuration and github actions for nerves_hub

--- config/prod.exs
+++ config/prod.exs
@@ -5,6 +5,12 @@ config :logger, level: :info
 
 config :nerves_hub, NervesHub.Repo, pool_size: 20
 config :nerves_hub, NervesHubWeb.DeviceEndpoint, server: true
-config :nerves_hub, NervesHubWeb.Endpoint, server: true
+
+config :nerves_hub, NervesHubWeb.Endpoint,
+  server: true,
+  force_ssl: [
+    rewrite_on: [:x_forwarded_proto],
+    exclude: [paths: ["/status/alive", "/status/_health", "/status/health", "/status/healthy"]]
+  ]
 
 config :phoenix, logger: false
